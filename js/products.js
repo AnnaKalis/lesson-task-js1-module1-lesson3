e18ddc53880a2165522bf885220b1845c3f9ccb5
AnnaKalis <annkalis@gmail.com>
2023-10-12 14:51:04 +0200

    Part 1 answers for task from module 1 lesson 3

--- js/products.js
+++ js/products.js
@@ -1,16 +1,40 @@
 const products = [
-    {
-        name: "Fancy Product",
-        price: 40,
-        soldOut: true,
-    },
-    {
-        name: "Another Product",
-        soldOut: false,
-    },
-    {
-        name: "Cheap Product",
-        price: 10,
-        soldOut: true,
-    },
+  {
+    name: "Fancy Product",
+    price: 30,
+    soldOut: true,
+  },
+  {
+    name: "Another Product",
+    soldOut: false,
+  },
+  {
+    name: "Very cheap Product",
+    price: 10,
+    soldOut: true,
+  },
 ];
+
+const container = document.querySelector(".container");
+
+let html = "";
+
+for (let i = 0; i < products.length; i++) {
+  let displayPrice = "Not available";
+  if (products[i].price) {
+    displayPrice = products[i].price;
+  }
+
+  let colour = "green";
+  if (products[i].soldOut){
+    colour = "red";
+  }
+
+  html += `
+    <div class="product">
+        <h4 style="color:${colour}">${products[i].name}</h4> 
+        <p>Price: ${displayPrice}</p>
+    </div>`;
+}
+
+container.innerHTML = html;
