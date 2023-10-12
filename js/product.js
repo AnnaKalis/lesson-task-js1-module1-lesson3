9b5e85864376a18657df5177c751b58cfae8bb38
AnnaKalis <annkalis@gmail.com>
2023-10-12 20:03:03 +0200

    Answers, displaying product properties

--- js/product.js
+++ js/product.js
@@ -1,5 +1,23 @@
 const product = {
-    name: "Fancy Product",
-    price: 99,
-    onSale: true,
+  name: "Fancy Product",
+  price: 99,
+  onSale: true,
 };
+
+const container = document.querySelector(".container");
+
+let displayPrice = "Not available";
+if (product.price) {
+  displayPrice = product.price;
+}
+
+let decoration = "none";
+if (product.onSale) {
+  decoration = "underline";
+}
+
+container.innerHTML += `
+    <div class="product">    
+        <h4 style="text-decoration: ${decoration}">${product.name}</h4>
+        <p>Price: ${displayPrice}</p>
+    </div>`;
